8126c8f3b9cad2145b2b0f6b6c72ec0eee28c0a7
Noam Levi <noaml12@hotmail.com>
2024-06-16 12:53:36 +0300

    starting dockerfile weight loss program

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ if [ ! -f /config/settings.yml ]; then
 fi
 
 concurrently --names=WEB,SERVER,AUTO-UPDATER --prefix-colors=auto --kill-others-on-fail \
-    "npm run start" \
+    "node server.js" \
     "NODE_ENV=production fastapi run src/app/api/main.py --port ${SERVER_PORT:-3001}" \
     "NODE_ENV=production sleep 5 && python -m src.app.api.auto_updater"
