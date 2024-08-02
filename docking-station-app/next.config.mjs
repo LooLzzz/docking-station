71ee9c8e4a5521bf78382d67b5171f9bac0f39c2
Noam Levi <noaml12@hotmail.com>
2024-08-02 12:52:25 +0300

    feat: enable nextjs pwa

--- docking-station-app/next.config.mjs
+++ docking-station-app/next.config.mjs
@@ -1,9 +1,37 @@
+import withPWA from '@ducanh2912/next-pwa'
+
 const NODE_ENV = process.env.NODE_ENV ?? 'development'
 const SERVER_PORT = process.env.SERVER_PORT ?? 3001
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+
+  webpack: (config) => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"))
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 
   rewrites: async () => {
     return [
@@ -23,4 +51,9 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: NODE_ENV === 'development',
+})(nextConfig)
--- docking-station-app/src/app/page.tsx
+++ docking-station-app/src/app/page.tsx
@@ -1,5 +1,3 @@
-import DockingStationLogo from '@/public/dockingstation-logo.png'
-
 import { CardsManager } from '@/components'
 import { Center } from '@mantine/core'
 import { Metadata } from 'next'
@@ -12,14 +10,43 @@ export default function Home() {
   )
 }
 
+const APP_NAME = 'Docking-Station'
+const APP_TITLE_TEMPLATE = `%s | ${APP_NAME}`
+const APP_DESCRIPTION = 'Docking Station for the 21st Century'
+
 export const metadata: Metadata = {
-  title: 'Docking Station',
-  icons: {
-    icon: [
-      {
-        rel: 'icon',
-        url: DockingStationLogo.src,
-      },
-    ],
+  applicationName: APP_NAME,
+
+  title: {
+    default: APP_NAME,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_NAME,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_NAME,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_NAME,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
   },
 }
